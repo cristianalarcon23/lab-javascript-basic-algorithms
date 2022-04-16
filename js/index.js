86ef77876509c8d64ddf0cd9806afec30db9a746
Cristian Alarcon <cristian.alarcon.rodriguez@gmail.com>
2022-04-16 22:47:34 +0200

    3.1 - 3.2 all solved

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-const hacker1 = "Set";
-const hacker2 = "Trau";
+let hacker1 = "Set";
+let hacker2 = "Trau";
 
 function greetUser1 () {
-    return(`The driver's name is ${hacker1}`);
-    console.log(`The driver's name is ${hacker1}`)
+    return (`The driver's name is ${hacker1}`);
 }
 
 function greetUser2 () {
-    return(`The navigator's name is ${hacker2}`);
-    console.log(`The navigator's name is ${hacker2}`);
+    return (`The navigator's name is ${hacker2}`);
 }
 
 console.log(greetUser1(hacker1));
@@ -17,48 +15,54 @@ console.log(greetUser2(hacker2));
 function userLength () {
     if (hacker1.length < hacker2.length) {
         return(`The driver has the longest name, it has ${hacker1.length} characters`);
-        console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
     }
     else if (hacker1.length > hacker2.length) {
         return(`The navigator has the longest name, it has ${hacker2.length} characters`);
-        console.log(`The navigator has the longest name, it has ${hacker2.length} characters`);
     }
     else {
         return("What? You both have same name")
-        console.log("What? You both have same name");
     }
 }
 
-userLength();
+console.log(userLength());
 
-function upperName (hacker1) {
+function upperName () {    
     return hacker1.toUpperCase();
-    console.log(hacker1.toUpperCase());
 }
 
-upperName();
+console.log(upperName())
 
 function upperNameSplit () {
   const upperSplit = upperName();
-  return upperSplit.split(" ");
+  return upperSplit.split("");
 }
 
-upperNameSplit();
+
+console.log(upperNameSplit());
+
+let newString = ""
+
+function reverseName (hacker2) {
+    for (let i = hacker2.length - 1; i >= 0; i--) 
+      { 
+      newString += hacker2[i]
+      }; 
+    return newString 
+    }
+
+
+console.log(reverseName(hacker2));
 
 function compareNames (hacker1, hacker2) {
     if (hacker1.localeCompare(hacker2) < 0) {
         return ("Driver's name go first")
     } 
+    else if (hacker1.localeCompare(hacker2) = 0) {
+        return ("You both have same names!")
+    }
     else {
         return ("Navigator's name goes first")
     }
 }
-console.log(compareNames(hacker1, hacker2));
+console.log(compareNames(hacker1,hacker2));
 
-function compareNamesEquals (hacker1, hacker2) {
-    if (hacker1.localeCompare(hacker2) = 0) {
-        return ("You both have same names!")
-    }
-}
-
-compareNamesEquals();
